d8e41be21a313c586478e086b851f813ee4da45b
collins <collins@eec534a6-9b32-0410-b620-99bfd6676575>
2007-02-23 23:23:00 +0000

    Added a serialize function to serialize calls to maybe-capswap.sh because if I start two shells at the same time they can both detect that capslock is not swapped, and then each swap it, accomplishing nothing.

--- scripts/maybe-capswap.sh
+++ scripts/maybe-capswap.sh
@@ -3,6 +3,21 @@
 # Only add :$PATH if $PATH is non empty.
 PATH=~/local/scripts${PATH:+:$PATH}
 
+# Pass in a command and have it serialized
+serialize () {
+    if which lockfile 2>/dev/null ; then
+        LOCKFILE=~/local/scripts/maybe-capswap.lock
+        # lockfile -sleeptime waits sleeptime seconds between retries
+        # when I lockfile is found.  Default was 8 seconds I think.
+        lockfile -1 $LOCKFILE
+        $1
+        rm -f $LOCKFILE
+    else
+        echo "lockfile command not on path, can't serialize."
+        echo "Running $1 anyway..."
+    fi
+}
+
 # Put caps lock in the right place.  I don't exactly understand what I
 # have done here.  From looking at the output of xmodmap I know that
 # 0x42 is what I don't want, and 0x25 is what I do want.  I am a
@@ -10,6 +25,7 @@ PATH=~/local/scripts${PATH:+:$PATH}
 # lock, and switching it if it is.  So this is probably nonsense, in
 # that it will break Sun keyboards that have control in the right
 # place to begin with.
+lambda () {
 if [ -n "$DISPLAY" ]; then
     if [ $(xmodmap | grep lock | awk '{ print $3 }') = '(0x42)' ]; then
 	echo Swapping control and caps lock
@@ -21,3 +37,6 @@ else
     echo \$DISPLAY not set, so can\'t use xmodmap to remap keys
     echo Control and caps lock are probably not properly mapped
 fi
+}
+
+serialize lambda
