fcdbe5978cfb31c4955c4a6b5104ec752641e534
nathan.collins <nathan.collins@eec534a6-9b32-0410-b620-99bfd6676575>
2008-06-18 21:32:10 +0000

    Added setup script.  The chicken/egg problem is solved by dling it from google.

---
+++ install-conf.py
@@ -0,0 +1,21 @@
+'''Run from your home dir.'''
+from subprocess import call
+from os import chdir, getcwd, getenv
+from os.path import exists
+
+def c(cmd):
+    call(cmd,shell=True)
+
+home = getcwd()
+if home != getenv('HOME'):
+    raise Exception('You should run this from your home dir.')
+c('mkdir versioned local')
+chdir('versioned')
+c('svn co https://nathan-collins--conf.googlecode.com/svn conf --username nathan.collins')
+for f in ('.emacs', '.pythonrc', '.screenrc', '.subversion', '.zshrc'):
+    from_ = '%(home)s/versioned/conf/dot%(f)s' % locals()
+    to = '%(home)s/%(f)s' % locals()
+    if exists(to):
+        c('mv %(to)s %(to)s.backup' % locals())
+    c('ln -s %(from_)s %(to)s'  % locals())
+c('ln -s %(home)s/versioned/conf/scripts %(home)s/local/' % locals())
