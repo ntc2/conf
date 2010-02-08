79ad5032451cbf98b97180087d3185e9692e86c5
Nathan Collins <nathan.collins@gmail.com>
2010-02-08 03:53:25 +0000

    use firefox to open org links

--- dot.emacs.d/ubuntu8.10-on-laptop.system-custom.el
+++ dot.emacs.d/ubuntu8.10-on-laptop.system-custom.el
@@ -6,3 +6,11 @@
 ;; which was bad ...
 (if (null (window-system))
     (set-face-background 'mode-line "yellow"))
+
+;; make org opened links (C-c C-o) open in firefox instead of galeon
+(setq browse-url-browser-function 'browse-url-firefox)
+;; enforce new window. better to just split out tab of first link
+;;opened into new window, then subsequent tabs open in that window if
+;;it had focus last.  
+
+;;(setq browse-url-new-window-flag t)
