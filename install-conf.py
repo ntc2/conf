43a0a65e3be692eb2565364f4619b41e6baabe5e
nathan.collins <nathan.collins@eec534a6-9b32-0410-b620-99bfd6676575>
2010-02-08 05:07:22 +0000

    updated install-conf.py to be 'more' idempotent (i.e., actually idempotent ...)

--- install-conf.py
+++ install-conf.py
@@ -28,19 +28,23 @@ def main():
     if not exists('conf'):
         c('svn co https://nathan-collins--conf.googlecode.com/svn conf --username nathan.collins')
 
+    # {ln -T} mean treat destination as a normal file, i.e. don't
+    # create file *in* target if target is a dir.  this is needed for
+    # the idempotence of {ln ~/versioned/conf/dot.zsh ~/.zsh}.
     for f in ('.emacs', '.pythonrc', '.screenrc', '.subversion/config', '.zsh'):
         from_ = '%(home)s/versioned/conf/dot%(f)s' % locals()
         to = '%(home)s/%(f)s' % locals()
 #         if exists(to):
 #             c('mv %(to)s %(to)s.backup' % locals())
-        c('ln -fs %(from_)s %(to)s'  % locals())
+        c('ln -Tfs %(from_)s %(to)s'  % locals())
     for f in ('.zshrc', '.zshenv'):
         from_ = '%(home)s/.zsh/dot%(f)s' % locals()
         to = '%(home)s/%(f)s' % locals()
-        c('ln -fs %(from_)s %(to)s'  % locals())
+        c('ln -Tfs %(from_)s %(to)s'  % locals())
 
     c('ln -fs %(home)s/versioned/conf/scripts %(home)s/local/' % locals())
-    
-    print """You may need to download versioned/docs and create a link from versioned/docs/scripts to ~/local/more-scripts."""
+
+    if not exists('%(home)s/local/more-scripts' % locals()):
+        print """You may need to download versioned/docs and create a link from versioned/docs/scripts to ~/local/more-scripts."""
 
 if __name__ == '__main__': main()
