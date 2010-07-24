96ea562ce9a49d27dd4882c4ba85e5f0c9c96db7
nathan.collins <nathan.collins@eec534a6-9b32-0410-b620-99bfd6676575>
2010-07-24 01:48:18 +0000

    versioning xmonad conf, not part of install.py

---
+++ xmonad.hs
@@ -0,0 +1,66 @@
+-- Gnome config from
+-- http://haskell.org/haskellwiki/Xmonad/Basic_Desktop_Environment_Integration
+-- see also
+-- http://donsbot.wordpress.com/2010/03/13/after-3-years-my-xmonad-configuration-now-uses-gnome/
+--
+-- need to set up gnome to use xmonad though
+-- http://haskell.org/haskellwiki/Xmonad/Using_xmonad_in_Gnome#Starting_GNOME_with_xmonad
+-- change the windowmanager used by gnome (and then logout, or start a
+-- new gnome with {startx -- :1} in a tty):
+-- 
+--   gconftool-2 -s /desktop/gnome/session/required_components/windowmanager xmonad --type string
+--   gconftool-2 -s /desktop/gnome/session/required_components/windowmanager metacity --type string
+--
+-- this gives multihead for free.
+--
+-- TODO
+--
+-- -  tabbing or stacking
+-- -  disable "smart" workspace switching, whereby xmonad swaps
+--    current workspace and target when both appear on monitors
+--    currently
+-- -  dzen or xmobar
+-- 
+-- DONE
+--
+-- -  {aptitude install dmenu}
+import XMonad
+import XMonad.Config.Gnome
+import XMonad.Actions.UpdatePointer -- mouse follows focus
+import XMonad.Layout.TabBarDecoration -- tabs: sucks by default: the tabs don't do anything :P
+-- import XMonad.Hooks.ManageHelpers -- fullscreen flash: fucks up everything else
+import System.Process -- use xmodmap to add another meta
+
+-- myManageHook = composeOne [ isFullscreen -?> doFullFloat ] 
+
+main = xmonad gnomeConfig
+       { logHook    = updatePointer (Relative 0.5 0.0) -- mouse
+         -- follows focus to middle-top i have two mod keys: the left
+         -- windows key (Super_L) and the right menu key (Menu)
+         -- 
+         -- - use xmodmap to get list of modifiers, e.g. Super_L is a
+         --   mod4 modifier.
+         --
+         -- - use xev to find keysym for a given key.
+         --
+         -- - use xmodmap to add keys to a modifier, and remap keys,
+         --   e.g. adding Menu to mod4:
+         --
+         --   : xmodmap -e "add Mod4 = Menu"
+         --   : xmodmap -e "keysym Menu = Super_L"
+         --
+         --   the second line alone may be enough, but I didn't do
+         --   things in that order ... YES!
+       , modMask     = mod4Mask
+       , startupHook = spawn "xmodmap -e \"keysym Menu = Super_L\""
+--       , manageHook = myManageHook <+> manageHook defaultConfig -- full-screen flash
+--       , layoutHook = simpleTabBar $ layoutHook defaultConfig
+       }
+
+-- http://hackage.haskell.org/packages/archive/xmonad-contrib/latest/doc/html/XMonad-Layout-Tabbed.html
+-- http://haskell.org/haskellwiki/Xmonad/Frequently_asked_questions#Multi_head_and_workspaces_.28desktops.29
+-- 
+
+-- complicated config, could be educational:
+--
+-- http://snipt.net/doitian/xmonad-configuration/
