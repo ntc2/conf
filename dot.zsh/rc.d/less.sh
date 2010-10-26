aaa145cd599cae643ad02be5d09c546629231f8f
Nathan Collins <nathan.collins@gmail.com>
2010-10-26 18:02:19 +0000

    svn native eols in trellys and vless in standard zsh config

--- dot.zsh/rc.d/less.sh
+++ dot.zsh/rc.d/less.sh
@@ -1,6 +1,12 @@
 # Interpret ascii escapes and show verbose status and ignore case in searches
 alias less="less -RMi"
 
+# get syntax highlighting in less, like on gentoo (doesn't work with
+# pipe though :P)
+#
+# sudo aptitude install vim-runtime vim-scripts (or vim-runtime vim-nox)
+alias vless='vim -u /usr/share/vim/vimcurrent/macros/less.vim'
+
 # standard
 if which lesspipe &>/dev/null; then
     eval $(lesspipe)
