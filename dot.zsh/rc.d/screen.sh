3c24a47f102b0caff2e19df74e7e9ca5769583f0
Nathan Collins <nathan.collins@gmail.com>
2011-10-14 14:27:33 +0000

    Added automatic setting of window titles in screen.

--- dot.zsh/rc.d/screen.sh
+++ dot.zsh/rc.d/screen.sh
@@ -2,3 +2,27 @@ alias fixssh='source ~/local/bin/fix-ssh.sh'
 alias genfixssh='source ~/local/scripts/gen-fix-ssh.sh'
 # screen doesn't see the alias defined by genfixssh?
 alias attach='genfixssh ; screen -dRR'
+
+# Set the title of the screen.
+#
+# See the 'TITLES (naming windows)' section of 'man screen' for more
+# info.  See http://aperiodic.net/phil/prompt/ for some much fancier
+# examples.
+#
+# Another strategy would be to always display the last command run,
+# and use a different screen facility to see a list of the current
+# directories of each screen ... the idea is to make is easy to know
+# which screen to switch to to continue what you were doing or to run
+# a new command.
+if [[ $TERM == "screen" ]]; then
+    precmd () {
+        : display current directory while waiting for commands
+        echo -ne "\ek$(basename $(pwd))/\e\\"
+        #'screen -X title <title>' also works ...
+    }
+
+    preexec () {
+        : display current command while it runs
+        echo -ne "\ek$1\e\\"
+    }
+fi
