90972d7c6c26a8a5e222fd826b39b26b2cdd7214
Nathan Collins <nathan.collins@gmail.com>
2012-04-17 20:58:49 +0000

    Fixed a subtle bug in my zsh preexec.
    I was using a 'print -P', which interprets percent escapes,
    and I was printing the current command, which could include
    arbitrary percent escapes.
    
    I ran into the problem when I tried to use
    
      date +%s
    
    on the command line, and got garbage on my screen.

--- dot.zsh/rc.d/xterm.sh
+++ dot.zsh/rc.d/xterm.sh
@@ -2,7 +2,22 @@
 case $TERM in
     *xterm*|rxvt|(dt|k|E)term)
         precmd () { print -Pn "\e]0;$(hostname | cut -d . -f 1):%~\a" }
-        preexec () { print -Pn "\e]0;$(hostname | cut -d . -f 1):$1\a" }
+        # Need to be careful with percent escapes in the command $1.
+        # Solution from
+
+        # http://www.zsh.org/mla/users/2010/msg00596.html
+
+        # The problem was that `print -P` expands percent escapes.  I
+        # don't have any percent escapes here, so I don't need -P.
+        #
+        # If I did also want percent expansion, I could use a fancier
+        # solution from
+
+        # https://bbs.archlinux.org/viewtopic.php?pid=960428#p960428
+
+        # I.e., use `${~1:gs/%/%%}` to escape the percents before
+        # expansion.
+        preexec () { printf "\e]0;$(hostname | cut -d . -f 1):$1\a" }
         ;;
 esac
 
