09babf3027bc1115f923c253e260e363c366110c
nathan.collins <nathan.collins@eec534a6-9b32-0410-b620-99bfd6676575>
2012-05-23 00:25:38 +0000

    Made nc:www-server command more robust: input validation and filename support.
    It shares the whole dir when given a single file name, which is confusing, but
    probably better than instead sharing the dir you ran the command in, after
    cd'ing into a file fails :P

--- dot.zsh/env.d/http.sh
+++ dot.zsh/env.d/http.sh
@@ -2,10 +2,21 @@
 # http://tips4linux.com/quick-and-easy-sharing-of-a-directory-over-http-in-linux/
 # although Rigo showed this trick to once upon a time.
 function nc:www-server () {
- : "usage $0 [DIR]"
+ : "usage $0 [DIR[/FILE]]"
  : "to start a webserver on port 8000 sharing DIR"
  (
-     if [[ $# -eq 1 ]]; then cd $1; fi
+     if [[ $# -eq 1 ]]; then
+         if [[ -f "$1" ]]; then
+             cd "$(dirname "$1")"
+         elif [[ -d "$1" ]]; then
+             cd "$1"
+         else
+             echo "error: not a path: $1" > /dev/stderr
+             echo "usage:" > /dev/stderr
+             type -f nc:www-server > /dev/stderr
+             exit 2
+         fi
+     fi
      python -c 'import SimpleHTTPServer;SimpleHTTPServer.test()'
  )
 }
