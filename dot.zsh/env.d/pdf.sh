bdcf9ab427560eba1c1c92b36b4da895bcb18069
nathan.collins <nathan.collins@eec534a6-9b32-0410-b620-99bfd6676575>
2012-07-04 04:28:28 +0000

    zsh: add 'nc:pdf:magic' for automated pdf rescaling.

---
+++ dot.zsh/env.d/pdf.sh
@@ -0,0 +1,57 @@
+nc:pdf:tutorial () {
+: Remind how to trim a pdf.  Optional arguments fill in some parts of generated commands.
+:
+: Use nc:pdf:magic instead!
+local watermark=${1-\$watermark} file="${2-\$file}"
+cat <<EOF
+usage: $0 [WATERMARK [FILE]]
+
+See ~/v/conf/scripts/pdf-rescalers/README.org for more details.
+
+pdfinfo "$file"
+nc:pdf:watermark $watermark "$file"
+evince "$file".wm
+# left bottom right top
+pdfnup --paper $watermark --nup 1x1 --trim "1.5cm 4.5cm 1.5cm 0cm" "$file"
+EOF
+}
+
+nc:pdf:magic () {
+: usage: $0 FILE
+:
+: Guess watermark, apply watermark, open watermarked file, 
+: and insert imcomplete 'pdfnup' command.
+[[ $# -eq 1 ]] && [[ -e "$1" ]] || return `(nc:usage $0)`
+local file="$1"
+
+local wm=$( pdfinfo "$file" \
+          | grep 'Page size:' \
+          | sed -re 's/.*\((.*?)\).*/\1/')
+
+local wmfile=$(nc:pdf:watermark $wm "$file")
+local nupfile=/tmp/$(basename "$file").pdfnup.pdf
+evince "$wmfile" &>/dev/null &
+
+local left bottom right top nup
+echo -n '<left> <bottom> <right> <top> (trims in cm): '
+read left bottom right top
+echo -n '<nup> (probably 2x1 or 1x1): '
+read nup
+# Insert pdfnup command into command line without running it.
+print -z \
+"pdfnup --paper $wm --nup $nup \
+--trim \"${left}cm ${bottom}cm ${right}cm ${top}cm\" \
+--outfile \"$nupfile\" \"$file\""
+}
+
+nc:pdf:watermark () {
+: usage: $0 WATERMARK FILE
+:
+: Apply a watermark to a pdf, to help calculate pdfnup trim params.
+local watermark=$1 file="$2"
+local wmfile=/tmp/$(basename "$file").wm
+pdftk "$file" \
+  background ~/v/conf/scripts/pdf-rescalers/watermark-$watermark.pdf \
+  output "$wmfile"
+  echo "$wmfile"
+}
