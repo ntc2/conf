25e0553b996725126922837d28b1f3a9a5b415cf
nathan.collins <nathan.collins@eec534a6-9b32-0410-b620-99bfd6676575>
2012-07-04 04:26:45 +0000

    zsh: factor 'nc:load-custom' into util.sh and add 'nc:usage'.

---
+++ dot.zsh/env.d/util.sh
@@ -0,0 +1,35 @@
+# This modules is sourced before others.
+
+function nc:load-custom {
+    : usage: $0 FILE
+    :
+    : Load FILE if it exists.
+    local FILE=$1
+    if [[ -e $FILE ]]; then
+	      source $FILE
+    fi
+}
+
+function nc:usage {
+    : 'usage: $0 FUNCTION [ERROR_MSG]'
+    :
+    : 'Print usage for FUNCTION and exit 2 (bad argument).'
+    :
+    : 'NB: the `exit` kills the calling shell, so this should'
+    : 'only be used in sub shells. To use in a function that'
+    : 'that does not spawn a sub shell, do:'
+    :
+    :     'return `(nc:usage ARGS)`'
+    :
+    : 'or'
+    :
+    :     'return `(nc:usage ARGS; echo $?)`'
+    fun="$1"
+    error_msg="$2"
+    if [[ -n "$error_msg" ]]; then
+        echo "error: $error_msg" > /dev/stderr
+        echo
+    fi
+    type -f "$fun" > /dev/stderr
+    exit 2
+}
