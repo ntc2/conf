9d940beac8e0fd5b491c18dffe1b7ae009fe06ac
Nathan Collins <nathan.collins@gmail.com>
2012-10-29 22:55:48 +0000

    Add emacs fallback when terminus font is not available.
    Hack ...

--- scripts/its-all-text-emacs.sh
+++ scripts/its-all-text-emacs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/usr/bin/emacs -font terminus --no-splash --fullscreen --reverse-video --no-desktop "$@"
+# Try again without '-font terminus' if the first version fails
+/usr/bin/emacs -font terminus --no-splash --fullscreen --reverse-video --no-desktop "$@" \
+|| /usr/bin/emacs --no-splash --fullscreen --reverse-video --no-desktop "$@"
