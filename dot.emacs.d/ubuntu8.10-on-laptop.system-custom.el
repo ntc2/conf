72742472c1a7a25b9442321a5f4cf22ce12940d8
nathan.collins <nathan.collins@eec534a6-9b32-0410-b620-99bfd6676575>
2013-04-10 05:07:03 +0000

    Comment out pointless emacs color settings.
    These probably mattered at some point, but they don't seem to do anything now.

--- dot.emacs.d/ubuntu8.10-on-laptop.system-custom.el
+++ dot.emacs.d/ubuntu8.10-on-laptop.system-custom.el
@@ -1,11 +1,11 @@
-;; manual reverse video
-(set-background-color "black")
-(set-foreground-color "#e5e5e5")
-(set-face-foreground 'mode-line "black")
-;; in terminal emacs the mode-line background was black in -rv mode,
-;; which was bad ...
-(if (null (window-system))
-    (set-face-background 'mode-line "yellow"))
+;; ;; manual reverse video
+;; (set-background-color "black")
+;; (set-foreground-color "#e5e5e5")
+;; (set-face-foreground 'mode-line "black")
+;; ;; in terminal emacs the mode-line background was black in -rv mode,
+;; ;; which was bad ...
+;; (if (null (window-system))
+;;     (set-face-background 'mode-line "yellow"))
 
 ;; make org opened links (C-c C-o) open in firefox instead of galeon
 (setq browse-url-browser-function 'browse-url-firefox)
