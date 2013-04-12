0335c5ca22fdf9796d1df2640a9336ba85bd4b85
Nathan Collins <nathan.collins@gmail.com>
2013-04-12 05:18:52 +0000

    Desktop Twelf config.

--- dot.emacs.d/ubuntu10.10-desktop.system-custom.el
+++ dot.emacs.d/ubuntu10.10-desktop.system-custom.el
@@ -1,3 +1,6 @@
 ;; from
 ;; http://stackoverflow.com/questions/1781762/enabling-flyspell-mode-gives-an-error
 (setq flyspell-issue-welcome-flag nil)
+
+;; twelf
+(setq twelf-root "/home/collins/v/twelf-spring-2013.git/bin/twelf/")
