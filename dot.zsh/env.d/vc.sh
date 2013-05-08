15ee060c767129b8b9edd4abbfb0efb2eaeb457a
Nathan Collins <nathan.collins@gmail.com>
2013-05-08 22:56:35 +0000

    Add command for disabling ws conversion in a Git repo.
    Disables whitespace conversion locally, i.e. in that working copy only.
    
    Also, fix doc bug in nc:svn:ls-junk (and rename).

--- dot.zsh/env.d/vc.sh
+++ dot.zsh/env.d/vc.sh
@@ -1,10 +1,22 @@
 ## Subversion
 
-function nc:svn:lsjunk {
-  : usage: $0 [PATH]
+function nc:svn:ls-junk {
+  : usage: $0 '[PATH]'
   :
   : Show untracked non-ignored files PATH, default current dir.
   :
   : Use with '| xargs rm' to delete junk.
   svn st "$@" | grep '^\?' | awk '{print $2}'
+}
+
+## Git
+
+function nc:git:disable-whitespace-conversion {
+  : usage: $0 PATH
+  :
+  : Disable whitespace-conversion related attributes on PATH, locally
+  : in the current repo.  "Absolute" path relative repo root is
+  : safest.
+
+  echo "$1 -text -whitespace" >> $(git rev-parse --git-dir)/info/attributes
 }
