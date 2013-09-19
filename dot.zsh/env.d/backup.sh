2efdc13bd2f46825f9ca74334a1ce6c2d797b0b8
nathan.collins <nathan.collins@eec534a6-9b32-0410-b620-99bfd6676575>
2013-09-19 18:52:50 +0000

    Add better argument checking to nc:mirror; add nc:git:mirror.

--- dot.zsh/env.d/backup.sh
+++ dot.zsh/env.d/backup.sh
@@ -3,11 +3,14 @@ function nc:mirror {
   : 'usage: $0 (FILE | DIR)'
   :
   : 'Copy (FILE | DIR) to ~/mirror/ on linuxlab.'
-  if [[ $# -ne 1 ]]; then
+  :
+  : 'If `basename DIR` is "." the name of dir will used for the backup.'
+  if [[ ! ($# -eq 1 && -e "$1") ]]; then
     return $(nc:usage nc:mirror "Wrong number of arguments.")
   fi
-  # Normalize path by removing any trailing slash.  Rsync behaves very
-  # differently with a trailing slash on source dirs!
-  local filepath=$(dirname "$1")/$(basename "$1")
-  rsync -avz --delete "$filepath" ntc2@linuxlab.cs.pdx.edu:mirror
+  # Normalize path expanding and removing any trailing slash.  Rsync
+  # behaves very differently with a trailing slash on source dirs!
+  local abspath=$(readlink -f "$1")
+  local normalpath=$(dirname "$abspath")/$(basename "$abspath")
+  rsync -avz --delete "$normalpath" ntc2@linuxlab.cs.pdx.edu:mirror
 }
