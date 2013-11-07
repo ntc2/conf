bba8ed194144714131722745aef1676c72549455
nathan.collins <nathan.collins@eec534a6-9b32-0410-b620-99bfd6676575>
2013-11-07 00:37:36 +0000

    Make renaming file and buffer easier.

---
+++ dot.emacs.d/extensions/dired.el
@@ -0,0 +1,6 @@
+;; Can now use C-x C-j followed R followed by C-x C-k to rename the
+;; current buffer and its underlying file.
+;;
+;; See
+;; http://stackoverflow.com/questions/384284/how-do-i-rename-an-open-file-in-emacs
+(require 'dired-x)
