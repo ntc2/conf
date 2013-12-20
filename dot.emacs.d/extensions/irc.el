d7d5cec37512f8bf8d21b495e094356f63fb542d
nathan.collins <nathan.collins@eec534a6-9b32-0410-b620-99bfd6676575>
2013-12-20 03:52:52 +0000

    Enable logging by default in Erc.

--- dot.emacs.d/extensions/irc.el
+++ dot.emacs.d/extensions/irc.el
@@ -1,2 +1,16 @@
 ;; Use fly-spell
 (erc-spelling-mode 1)
+
+;; Log all irc activity.
+;;
+;; See http://www.emacswiki.org/emacs/ErcLogging#toc5.
+;;
+;; Logs go in ~/log by default.
+;;
+;; After 'erc-save-buffer-in-logs' has run at least once, can run
+;; again with 'C-cC-l'.
+;;
+;; Saves log on every insert into the Erc buffer. Here "insert" means
+;; "change what is visible", so the buffer gets saved on every new
+;; message, not just messages typed by me.
+(add-hook 'erc-insert-post-hook 'erc-save-buffer-in-logs)
