87863811de701fc09a4304496620f194cb6c3355
Nathan Collins <nathan.collins@gmail.com>
2014-04-19 05:28:45 +0000

    Make nc:pdf:magic work with non standard paper sizes.

---
+++ scripts/pdf-rescalers/make-watermark.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+# Usage: $0 <width in big points> <height in big points>
+#
+# Create a watermark pdf of the given width and height.
+
+width="$1"
+height="$2"
+
+prefix="/tmp/watermark-$width-$height"
+
+# NB: Latex defines a 'pt' differently than a 'bp' (big point). We
+# want big points.
+cat <<EOF > "$prefix".tex
+\documentclass{article}
+\usepackage[paperwidth=${width}bp, paperheight=${height}bp]{geometry}
+\input{watermark.tex}
+EOF
+
+TEXINPUTS="$(dirname "$0")/tex//:" pdflatex \
+  -output-directory="$(dirname "$prefix")" \
+  "$prefix".tex &>/dev/stderr
+
+echo "$prefix".pdf
