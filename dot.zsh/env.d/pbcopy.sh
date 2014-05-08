60bc3e391b265186e1bbe432dfaa1043ad208ac7
nathan.collins <nathan.collins@eec534a6-9b32-0410-b620-99bfd6676575>
2014-05-08 03:04:48 +0000

    Add more pbcopy/pbaste commands.
    What I really want is for Firefox C-c to copy to *all*
    clipboards.

--- dot.zsh/env.d/pbcopy.sh
+++ dot.zsh/env.d/pbcopy.sh
@@ -8,8 +8,20 @@
 # alias pbcopy='xsel --clipboard --input'
 # alias pbpaste='xsel --clipboard --output'
 
-# Use both the C-c / C-v and the highlight / middle mouse clipboard
-# when saving; read from the highlight clipboard (but, since you
-# generally highlight before doing C-c, this works for both!):
-alias nc:pbcopy='tee >(xsel --input --primary) | xsel --input --clipboard'
-alias nc:pbpaste='xsel --output --primary'
+# Save to both the C-c / C-v clipboard (called '--clipboard') and the
+# highlight / middle mouse clipboard (called '--primary') when copying
+# stdin.
+alias nc:pbcopy:stdin='tee >(xsel --input --primary) | xsel --input --clipboard'
+# Separate commands for reading from the highlight clipboard and C-c /
+# C-v clipboard (since you generally highlight before doing C-c, the
+# 'nc:pbpaste:primary' often works for both, but not from Firefox to
+# Emacs in some fancy Javascripty text fields ... since that's the
+# only case I seem to care about in practice, a better solution would
+# be probably to ensure that C-c in Firefox copied to all clipboards
+# ...):
+alias nc:pbpaste:primary='xsel --output --primary'
+alias nc:pbpaste:clipboard='xsel --output --clipboard'
+# Separate commands for duplicating the highlight and C-c / C-v
+# clipboards to both clipboards.
+alias nc:pbcopy:primary='nc:pbpaste:primary | nc:pbcopy:stdin'
+alias nc:pbcopy:clipboard='nc:pbpaste:clipboard | nc:pbcopy:stdin'
