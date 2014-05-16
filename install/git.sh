001a2451b7d23a9f48d74f02aff1a5025cd7ab45
Nathan Collins <nathan.collins@gmail.com>
2014-05-16 03:18:52 +0000

    Add notes on curl to Git installer.
    Basically, uncomment the 'flair' variable to use local curl, or no curl
    at all, and remove default 'sudo aptitude install libcurl4...'.
    
    Only add Git line to shell conf if not already present.

--- install/git.sh
+++ install/git.sh
@@ -24,12 +24,52 @@ cd git-${release}
 # corresponding header files.  NOTE: if libcurl-dev is not installed,
 # then git is built without http(s) support.  There are multiple
 # versions of libcurl-dev, so we have to choose one :P
-sudo aptitude install libcurl4-openssl-dev
+
+
+################################################################
+# libcurl
+#
+# If libcurl is not installed and I have root:
+#
+# $ sudo aptitude install libcurl4-openssl-dev
+#
+# If I don't have root, build w/o curl support (so no http(s) URLs):
+#
+#flair="NO_CURL=1"
+#
+# Or, install curl locallys:
+#
+# $ ./curl.sh
+#
+# and then:
+#
+# (I just had my mind blown: in zsh, strings with spaces are preserved
+# when substituted, unless 'SH_WORD_SPLIT' is set! This explains some
+# confusion I've had in the past ... however, using arrays (space
+# separated values in parens) in zsh makes splitting happen as in bash
+# and sh. Splitting is important for the 'flair' variable!
+# http://zsh.sourceforge.net/FAQ/zshfaq03.html)
+#
+# The 'NO_R_TO_GCC_LINKER' is an obscure option I found grepping the
+# Git Makefile after getting an error about "cc: ... unrecognized -R"
+# or something. Only happens when 'CURLDIR' is set.
+#
+# Also, unsurprisingly but causing me confusion: the tilde is not
+# expanded after the equal sign in
+# 'CURLDIR=~/local/opt/curl-7.36.0'. This makes sense: tildes
+# shouldn't be expanded arbitrarily. The confusing part is that if the
+# shell is were interpreting that assignment, then the tilde would be
+# expanded. Tricky tricky ...
+#
+#flair=(NO_R_TO_GCC_LINKER=1 CURLDIR=$HOME/local/opt/curl-7.36.0)
+#
+################################################################
+
 mkdir -p ~/local/opt
 # make configure
 # ./configure --prefix=$(readlink -f $prefix)
-make prefix=$(readlink -f $prefix)
-make prefix=$(readlink -f $prefix) install
+make -j $flair prefix=$(readlink -f $prefix) && \
+make -j $flair prefix=$(readlink -f $prefix) install
 
 # The docs can be built, but that is slow, and requires asciidoc and
 # xmlto, which are not installed on linuxlab machines.  Instead, just
@@ -53,12 +93,17 @@ wget $url/git-manpages-${release}.tar.gz -O- | tar xzf -)
 cp -r contrib $prefix
 (cd $prefix/contrib/completion && ln -fs git-completion.zsh _git)
 
-customize () {
-    echo $@ >> ~/.zshenv.system-custom
-}
-customize ''
-customize '# Git'
-customize 'export PATH=~/local/opt/git/bin:$PATH'
-customize 'export FPATH=~/local/opt/git/contrib/completion:$FPATH'
+if ! grep -q '^# Git$' ~/.zshenv.system-custom; then
 
-print -P '%K{red}YOUR ~/.zshenv.system-custom HAS BEEN UPDATED!%k'
+    customize () {
+	echo $@ >> ~/.zshenv.system-custom
+    }
+    customize ''
+    customize '# Git'
+    customize 'export PATH=~/local/opt/git/bin:$PATH'
+    customize 'export FPATH=~/local/opt/git/contrib/completion:$FPATH'
+
+    print -P '%K{red}YOUR ~/.zshenv.system-custom HAS BEEN UPDATED!%k'
+else
+    print -P '%K{red}YOUR ~/.zshenv.system-custom already has custom Git%k'
+fi
