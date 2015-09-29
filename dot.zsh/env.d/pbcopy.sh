5174358234493e4e540c6260144d9f05f065d45e
Nathan Collins <conathan@galois.com>
2015-09-29 15:47:36 -0700

    Tweak clipboard manipulation commands.
    I'm having a lot of trouble with copy-by-highlight copies only being
    pastable by middle click. I don't remember having this problem so much
    in the past. I might need to start using a clipboard manager like
    Parcellite.

--- dot.zsh/env.d/pbcopy.sh
+++ dot.zsh/env.d/pbcopy.sh
@@ -11,6 +11,12 @@
 # Save to both the C-c / C-v clipboard (called '--clipboard') and the
 # highlight / middle mouse clipboard (called '--primary') when copying
 # stdin.
+#
+# UPDATE: this just hangs for me now; probably related to some ZSH
+# change I made since defining this. However, it was only used to copy
+# one clibboard to another, so below I just replaced it with
+# overwriting the destination clipboard, since there's no point in
+# overwriting the source clipboard with it's current content.
 alias nc:pbcopy:stdin='tee >(xsel --input --primary) | xsel --input --clipboard'
 # Separate commands for reading from the highlight clipboard and C-c /
 # C-v clipboard (since you generally highlight before doing C-c, the
@@ -19,9 +25,9 @@ alias nc:pbcopy:stdin='tee >(xsel --input --primary) | xsel --input --clipboard'
 # only case I seem to care about in practice, a better solution would
 # be probably to ensure that C-c in Firefox copied to all clipboards
 # ...):
-alias nc:pbpaste:primary='xsel --output --primary'
-alias nc:pbpaste:clipboard='xsel --output --clipboard'
+alias nc:pbpaste:highlight='xsel --output --primary'
+alias nc:pbpaste:ctrlc='xsel --output --clipboard'
 # Separate commands for duplicating the highlight and C-c / C-v
 # clipboards to both clipboards.
-alias nc:pbcopy:primary='nc:pbpaste:primary | nc:pbcopy:stdin'
-alias nc:pbcopy:clipboard='nc:pbpaste:clipboard | nc:pbcopy:stdin'
+alias nc:pbcopy:highlight='nc:pbpaste:highlight | xsel --input --clipboard'
+alias nc:pbcopy:ctrlc='nc:pbpaste:clipboard | xsel --input --primary'
