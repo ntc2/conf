140cbfead971f1bf0f49786bbfb7b0a60191de2f
Nathan Collins <conathan@galois.com>
2015-11-25 17:45:46 -0800

    ZSH: support rsync options with `nc:mirror:put`.
    I wanted to `--exclude` and `--delete-excluded` a bunch
    of build dirs to save space on the backup host.

--- dot.zsh/env.d/backup.sh
+++ dot.zsh/env.d/backup.sh
@@ -7,32 +7,34 @@ BACKUP_USER=ntc2
 
 # Mirror a file or directory on linuxlab
 function nc:mirror:put {
-  : 'usage: $0 (FILE | DIR)'
+  : 'usage: $0 (FILE | DIR) [RSYNC_OPTS]'
   :
   : 'Copy (FILE | DIR) to ~/mirror/ on $BACKUP_HOST.'
   : 'Structure of mirror is duplicated in ~/mirror-structure'
   : 'to ease exploration.'
   :
   : 'If `basename DIR` is "." the name of dir will used for the backup.'
-  if [[ ! ($# -eq 1) ]]; then
-    return $(nc:usage nc:mirror:put "Wrong number of arguments.")
+  if [[ $# -eq 0 ]]; then
+    return $(nc:usage nc:mirror:put "You must specify a file or dir!")
   fi
-  if [[ ! (-e "$1") ]]; then
+  file="$1"; shift
+  if [[ ! (-e "$file") ]]; then
     return $(nc:usage nc:mirror:put "Source '$1' does not exist.")
   fi
   ssh $BACKUP_USER@$BACKUP_HOST "mkdir -p ~/mirror"
   # Normalize path by expanding and then removing any trailing slash.
   # Rsync behaves very differently with a trailing slash on source
   # dirs!
-  local abspath=$(readlink -f "$1")
+  local abspath=$(readlink -f "$file")
   local normalpath=$(dirname "$abspath")/$(basename "$abspath")
+  echo "Mirroring $file ..."
   # The '--relative' options means to recreate the full source path
   # under the target directory at the destination. The '--delete'
   # option means to delete files on the destination which are not on
   # the host, but only for *directories* that are explicitly
   # synced. So, e.g, separate syncs of 'foo/a' and 'foo/b' will not
   # result in only having 'b' in 'foo' on the destination host.
-  rsync -avz --relative --human-readable --delete "$normalpath" \
+  rsync -avz --relative --human-readable --delete "$@" "$normalpath" \
     $BACKUP_USER@$BACKUP_HOST:mirror/$(hostname -f)/
   # Note what was mirrored, to making exploring the mirror later
   # easier.  Encode the slashes as '<slash>' since they aren't allowed
