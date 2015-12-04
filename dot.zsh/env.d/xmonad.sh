953623bee64be8a66410b635b34263c03558bdc8
Nathan Collins <conathan@galois.com>
2015-12-03 19:58:35 -0800

    Split XMonad shell hacks into separate functions.
    I use the keyboard hack regularly everywhere, but not the Gnome panel
    hack.

--- dot.zsh/env.d/xmonad.sh
+++ dot.zsh/env.d/xmonad.sh
@@ -1,8 +1,13 @@
-function nc:xmonad:apply-hacks {
-  : 'usage: Apply XMonad hacks. See `~/.xsession` for details.'
-  # This segfaults, but not before fixing the struts (gaps for Gnome
-  # panel).
-  mutter
+function nc:xmonad:hack-keyboard {
+  : 'See `~/.xsession` for details.'
   # Make the menu key another XMonad meta key.
   xmodmap -e "keysym Menu = Super_R"
+}
+
+function nc:xmonad:hack-gnome-panel {
+  : 'See `~/.xsession` for details.'
+  # This segfaults, but not before fixing the struts (gaps for Gnome
+  # panel). However, it does move the windows around a little in the
+  # process.
+  mutter
 }
