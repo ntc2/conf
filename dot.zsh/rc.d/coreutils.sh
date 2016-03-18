9c1e0709cd40b453215aa48ba45165ece6a25d00
Nathan Collins <conathan@galois.com>
2016-03-18 11:59:16 -0700

    ZSH: add `nc:man` command for wide terminals.

--- dot.zsh/rc.d/coreutils.sh
+++ dot.zsh/rc.d/coreutils.sh
@@ -1,3 +1,7 @@
+# Man page fixed at 65 columns. Full width man pages in a wide
+# terminal are hard to read.
+alias nc:man="MANWIDTH=65 man"
+
 # Historical comment :)
 
 # #10/1/04 a good ol` fashion bashrc
