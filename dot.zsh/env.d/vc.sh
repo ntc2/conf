93685056a2f3f413ce433e5420719698d9d9d55a
Nathan Collins <conathan@galois.com>
2016-04-05 12:50:02 -0700

    ZSH: Remove `nc:git:clone` function.
    I rarely use this and it can be simulated easily using history
    references. I.e.
    
        git <URL>/<repo>.git <local path>/!#:$:t
    
    expands to
    
        git <URL>/<repo>.git <local path>/<repo>.git
    
    So, just need to remember `!#:$:t` :D Explanation:
    
        !# = current command
        :$ = last argument
        :t = basename ("tail")

--- dot.zsh/env.d/vc.sh
+++ dot.zsh/env.d/vc.sh
@@ -49,32 +49,3 @@ function nc:git:mirror {
   : 'Mirror the current Git repo.'
   nc:mirror:put $(git rev-parse --show-toplevel)
 }
-
-function nc:git:clone {
-  : 'usage: $0 GIT_CLONE_URL [PARENT]'
-  :
-  : 'Clone a git repo into parent dir PARENT (default .) with repo dir'
-  : 'derived from the GIT_CLONE_URL, *with* the .git at the end.'
-  :
-  : 'For example:'
-  :
-  :   '$0 user@www.example.com:user/path/to/repo.git parent'
-  :
-  : 'will clone into'
-  :
-  :   'parent/repo.git'
-  if (($# < 1 || $# > 2)); then
-    return $(nc:usage nc:git:clone "Wrong number of arguments.")
-  fi
-
-  url="$1"
-  repo="$(basename "$url")"
-  if (($# == 1)); then
-    parent="."
-  else
-    parent="$2"
-  fi
-
-  mkdir -p "$parent"
-  git clone "$url" "$parent"/"$repo"
-}
