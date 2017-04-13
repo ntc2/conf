41dd06e2973782c720a5656cdcd6df1bc59da298
Nathan Collins <conathan@galois.com>
2017-04-13 11:05:55 -0700

    ZSH: add alias `open = xdg-open`.

---
+++ dot.zsh/env.d/misc.sh
@@ -0,0 +1,6 @@
+# Miscellaneous things that don't warrant their own category. Could
+# put these in the top-level dot.zshenv instead.
+
+# Open a file using the "right" program automatically. Note that this
+# even with urls: it opens them in your browser.
+alias open=xdg-open
