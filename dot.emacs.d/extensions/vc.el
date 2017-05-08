1aad2165378913e522c8415ab4b4119a0e99072d
Nathan Collins <conathan@galois.com>
2017-05-07 22:42:11 -0700

    Emacs: add `magit-refresh-all` shortcut.
    And change prefix for Magit shortcuts to `C-c m`. I think there's a
    way to mark a prefix as such, to make `C-h k` or similar work better
    after typing the prefix ...

--- dot.emacs.d/extensions/vc.el
+++ dot.emacs.d/extensions/vc.el
@@ -5,4 +5,9 @@
 
 ;; As suggested in https://magit.vc/manual/magit/Status-buffer.html,
 ;; so I can stop typing 'M-x magit-status RET'.
-(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-c m s") 'magit-status)
+
+;; My `git-gutter' doesn't update properly after commits, but this
+;; seems to do the trick (but why can't I make this happen
+;; automatically ...).
+(global-set-key (kbd "C-c m r") 'magit-refresh-all)
