62a8536984bc9af00ae176d064fe19663fd52232
Nathan Collins <conathan@galois.com>
2018-07-02 12:14:22 -0700

    Emacs: version work system-custom.el

---
+++ dot.emacs.d/galois-office.system-custom.el
@@ -0,0 +1,6 @@
+(desktop-save-mode 1)
+(setq history-length 250)
+(add-to-list 'desktop-globals-to-save 'file-name-history)
+
+;; Use my modified haskell-mode until my patch is accepted.
+(add-to-list 'load-path "~/v/haskell-mode.fork.git/")
