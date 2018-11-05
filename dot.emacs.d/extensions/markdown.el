c185e683e87e88a6e872b2761a4e37ce53d4e5db
Nathan Collins <conathan@galois.com>
2018-11-05 13:10:21 -0800

    Emacs: tweak `markdown-mode` config
    I was getting an error about `poly-markdown` package not being
    available, and these tweaks resolved the problem. The last thing I did
    was reverse the order of the `require`s for `markdown-mode` and
    `polymode`. I don't know why that should matter.

--- dot.emacs.d/extensions/markdown.el
+++ dot.emacs.d/extensions/markdown.el
@@ -1,5 +1,8 @@
 (package-initialize)
-(require 'markdown-mode)
 (require 'polymode)
+(require 'markdown-mode)
+;; Requiring `poly-markdown' here doesn't seem to be necessary when
+;; `poly-markdown' is installed, but it will fail loudly if
+;; `poly-markdown' is not installed.
 (require 'poly-markdown)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . poly-markdown-mode))
