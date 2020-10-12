860cf95b57075ac6c48412daa371dd6a284c070d
Nathan Collins <nathan.collins@gmail.com>
2020-10-12 17:55:14 +0800

    [Python] Config Emacs doc comment filling
    And remove a "Use Ipython!" message in my .pythonrc.

---
+++ dot.emacs.d/extensions/python.el
@@ -0,0 +1,6 @@
+(use-package python-mode
+  :commands python-mode
+  :custom
+  (python-fill-docstring-style nil "Fill doctrings as tight as
+  possible. This saves two lines of screen space vs the default
+  setting for multiline doc strings!"))
