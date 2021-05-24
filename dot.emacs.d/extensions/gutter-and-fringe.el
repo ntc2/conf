ebb3f1ec66553d575f96448dc5752521a51d899f
Nathan Collins <nathan.collins@gmail.com>
2021-05-24 15:01:48 +0800

    [Emacs] Move from git-gutter-fringe to diff-hl, switch line nums
    The git-gutter-fringe stopped working for me in Emacs 27 and diff-hl
    seems to do everything git-gutter-fringe did.
    
    Also, switch from linum-mode to display-line-numbers-mode in Emacs
    26+. This works in org-mode, unlike linum mode.

--- dot.emacs.d/extensions/gutter-and-fringe.el
+++ dot.emacs.d/extensions/gutter-and-fringe.el
@@ -1,42 +1,27 @@
 ;; Perhaps this file is poorly named: it's named by what part of the
 ;; Emacs UI it affects, not by what it actually does ...
 
-;; Enable linum-mode and git-gutter-mode minor modes in all non-org
-;; major modes. Using the same hack as in ./flyspell.el -- i.e. piggy
-;; backing on font lock hook -- to do run this in essentially all
-;; major modes that correspond to text editing.
-;;
-;; It might be OK to use git-gutter-mode with org-mode; I haven't
-;; tested, but folding surely can't make sense. This hack doesn't work
-;; very consistently ... perhaps font lock starts before org mode
-;; sometimes?
-(require 'git-gutter-fringe nil t)
-(add-hook 'font-lock-mode-hook
-          (lambda ()
-            (when (not (member major-mode '(org-mode)))
-              (git-gutter-mode 1))))
+;; Like git-gutter, but that stopped working for me in Emacs 27. The
+;; commands are prefixed with `C-x v', e.g. `C-x v =' shows the diff
+;; for the current file, focusing on the current hunk.
+(global-diff-hl-mode)
 
 ;; Make it easy to quickly jump between modified sections of the
 ;; file. Using some kind of outline/folding mode might be a more
 ;; general way to achieve this kind of navigation.
-(global-set-key (kbd "M-<prior>") 'git-gutter:previous-hunk)
-(global-set-key (kbd "M-<next>")  'git-gutter:next-hunk)
+(global-set-key (kbd "M-<prior>") 'diff-hl-previous-hunk)
+(global-set-key (kbd "M-<next>")  'diff-hl-next-hunk)
 
 ;; Use `linum-mode' in all programming modes, except `org-mode'.
-(add-hook 'prog-mode-hook 'linum-mode)
-(add-hook 'org-mode-hook (lambda () (linum-mode 0)))
+(if (version<= "26.0.50" emacs-version )
+    ;; In newer versions of emacs use new display-line-numbers
+    ;; everywhere, since it doesn't slow down org-mode.
+    (global-display-line-numbers-mode)
+  (progn
+    (add-hook 'prog-mode-hook 'linum-mode)
+    (add-hook 'org-mode-hook (lambda () (linum-mode 0)))
+    (add-hook 'text-mode-hook 'linum-mode)))
 
-(add-hook 'text-mode-hook 'linum-mode)
-
-;; Make git-gutter work with magit. By default, when committing from
-;; magit-status, git-gutter doesn't realize a commit has been made.
-;;
-;; See
-;; http://stackoverflow.com/questions/23344540/emacs-update-git-gutter-annotations-when-staging-or-unstaging-changes-in-magit.
-;;
-;; Another choice for a Git gutter is diff-hl-mode:
-;; https://github.com/dgutov/diff-hl. Haven't tried that one yet.
-(require 'git-gutter-fringe nil t)
 (require 'magit nil t)
-(add-hook 'magit-post-refresh-hook
-          #'git-gutter:update-all-windows)
+(add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
+(add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
