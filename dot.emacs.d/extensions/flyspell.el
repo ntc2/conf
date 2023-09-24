bbd1be5d89586835614963c9ec3a66227045e552
Nathan Collins <nathan.collins@gmail.com>
2023-09-24 16:43:12 -0700

    [Emacs] Bind C-: to flyspell-auto-correct-previous-word
    For many years I've been accessing this via C-;, the default binding,
    but when I upgraded to Ubuntu 22.04 that broke :/ Seems something is
    grabbing that binding before it makes it to Emacs, but I can't figure
    out what.

--- dot.emacs.d/extensions/flyspell.el
+++ dot.emacs.d/extensions/flyspell.el
@@ -1,32 +1,14 @@
-(require 'flyspell)
-
-;; Optionally disable `flyspell`.
-;;
-;; http://www.math.utah.edu/docs/info/elisp_11.html#SEC127 
-;;
-;; The value specied for a defvar is only used if the variable is
-;; undefined when the defvar is evaluated.
-(defvar nc:use-flyspell t
-  "True if flyspell should be used.  Flyspell causes errors when
-  neither of ispell or aspell are installed.")
-
-;(add-hook 'font-lock-mode-hook 'flyspell-prog-mode)
-;(add-hook 'text-mode-hook 'flyspell-mode)
-
-;; Tell `flyspell` to not bind `M-TAB`, which is used for completion
-;; by default.
-;;
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Symbol-Completion.html#Symbol-Completion
-;;
-;; Flyspell actually provides a special way to do this:
-;;
-(nc:custom-set-variable flyspell-use-meta-tab nil)
-;;
-;; but here is a general way:
-;;
-;;(eval-after-load 'flyspell '(define-key flyspell-mode-map "\M-\t" nil))
-
-;; (add-hook 'font-lock-mode-hook (lambda () (flyspell-mode 1)))
+(use-package flyspell
+  :bind
+  ;; This is bound to `C-;' already, but since I upgraded to Ubuntu
+  ;; 22.04 something prevents that key from making it to Emacs.
+  (("C-:" . flyspell-auto-correct-previous-word))
+  :custom
+  ;; Tell `flyspell` to not bind `M-TAB`, which is used for completion
+  ;; by default.
+  ;;
+  ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Symbol-Completion.html#Symbol-Completion
+  (flyspell-use-meta-tab nil))
 
 ;; Enable `flyspell-mode` or `flyspell-prog-mode` selectively. I
 ;; separately enable `flyspell-prog-mode` in `./haskell.el` for
