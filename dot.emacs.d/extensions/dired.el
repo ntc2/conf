17769c6ebc687b63afd06cfd54d880b126616b0e
Nathan Collins <nathan.collins@gmail.com>
2023-09-27 12:30:16 -0700

    [Emacs] setup treemacs, a tree layout file explorer

--- dot.emacs.d/extensions/dired.el
+++ dot.emacs.d/extensions/dired.el
@@ -4,3 +4,42 @@
 ;; See
 ;; http://stackoverflow.com/questions/384284/how-do-i-rename-an-open-file-in-emacs
 (require 'dired-x)
+
+(defun nc/treemacs-quit ()
+  "Workaround for buggy treemacs-quit function when in peek mode."
+  (interactive)
+  (treemacs-peek-mode -1)
+  (treemacs-quit))
+
+;; Treemacs: graphical tree based file browser
+;;
+;; The docs are good:
+;; - customization vars: https://github.com/Alexander-Miller/treemacs
+;; - unbound functions: https://github.com/Alexander-Miller/treemacs#unbound-functions
+;; - default keys: https://github.com/Alexander-Miller/treemacs#default-keymaps
+(use-package treemacs
+  :ensure t
+  :defer t
+  :pin "melpa-stable"
+  :bind (("C-c t" . treemacs)
+         (:map treemacs-mode-map
+               ;; By default q is bound to treemacs-quit, but that
+               ;; doesn't do the right thing with temp buffers in peek
+               ;; mode. See
+               ;; https://github.com/Alexander-Miller/treemacs/issues/1063
+               ("q" . nc/treemacs-quit)))
+  :config
+  ;; Peek mode toggle is already bound to P by default.
+  (add-hook 'treemacs-mode-hook (lambda () (treemacs-peek-mode 1))))
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t
+  :defer t
+  :pin "melpa-stable")
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t
+  :defer t
+  :pin "melpa-stable")
