4ac29f4d54d6f763272b310e1f437efb6413d23a
Nathan Collins <nathan.collins@gmail.com>
2024-09-14 20:06:49 +0800

    [Emacs] Move diff-hl conf
    Because it needs to load after magit. Before that was happening via a top-level
    magit load in 00-dependencies.el, but I removed that because it seems that
    having multiple use-package declarations for the same package causes problems
    with `:custom` settings.

--- dot.emacs.d/extensions/gutter-and-fringe.el
+++ dot.emacs.d/extensions/gutter-and-fringe.el
@@ -1,27 +1,7 @@
 ;; Perhaps this file is poorly named: it's named by what part of the
 ;; Emacs UI it affects, not by what it actually does ...
 
-;; Like git-gutter, but that stopped working for me in Emacs 27. The
-;; commands are prefixed with `C-x v', e.g. `C-x v =' shows the diff
-;; for the current file, focusing on the current hunk.
-(use-package diff-hl
-  :requires magit
-  ;; Make it easy to quickly jump between modified sections of the
-  ;; file. Using some kind of outline/folding mode might be a more
-  ;; general way to achieve this kind of navigation.
-  :bind
-  (("M-<prior>" . diff-hl-previous-hunk)
-   ("M-<next>" . diff-hl-next-hunk))
-  :init
-  (global-diff-hl-mode 1)
-  (global-diff-hl-show-hunk-mouse-mode 1)
-  (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
-  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
-  ;; The default color for changed lines looks almost the same as the default
-  ;; color for added lines, so change to something more distinct. Emacs color
-  ;; list: https://www.raebear.net/computers/emacs-colors/
-  (custom-set-faces
-  '(diff-hl-change ((t (:foreground "sky blue" :background "dodger blue"))))))
+;; See vc.el for diff-hl configuration, which needs to load after magit.
 
 ;; Disable scroll bars: takes up space.
 (scroll-bar-mode 0)
